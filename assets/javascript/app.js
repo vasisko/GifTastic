b3f2e63b7f3c62221eaad0d8a374783b09bbc558
Carolyn Vasisko <carolyn@mothermedia.com>
2018-03-05 11:57:56 -0600

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// GifTastic Javascript
+
+$(document).ready(function(){
+
+//initialize var's
+
+// create array of strings
+    // var topics = [  ]
+
+// Function:  displayButtons
+//add buttons to page dynamically:  loop through array creating a button for each element in array
+
+
+
+// * append new <button> to <div> containing buttons
+// * add class to button tag using .addClass
+// * add attribute to tag to add data-value = image name 
+
+//get user input:  what type of gif requested?
+
+//remove spaces/blanks
+
+// ajax query Giphy : send request -- GET 10 images of chosen keyword
+
+// React to user input:  
+
+// 1. Receive/process returned Giphy data -- display 10 images along with rating
+
+// if animated gif:  
+    // user click to start animation -- continue as infinite loop
+    // user click to end
+
+// 2. Add user keyword to topics array and add to buttons (call displayButtons)
+
+});
