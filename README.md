2d210102786013b445c97cdc9d7c6f93584c8b03
Carolyn Vasisko <carolyn@mothermedia.com>
2018-03-07 14:16:44 -0600

    styled page and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GifTastic
+
+
+Source for animated gifs, powered by Giphy.
+User can select one of the topics or enter their own topic
+Up to 10 gifs are returned.
+
+To use: 
+Select a button to view animated gifs that pertain to that topic.
+Or - enter a keyword/phrase into the textbox to add a button with your desired topic that you can select.
+
+The results are returned on the page below the buttons, displaying up to 10 images along with the image's rating(g or pg ).
+
+To animate gifs:  click on gif to start animation -- continue as infinite loop, and click to stop
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,34 +2,19 @@
 
 $(document).ready(function(){
 
-//initialize var's
-var newGiphy="";
-// create array of topics
+    // Initialize var's
+    var newGiphy="";
+
+    // Create initial array of topics
     var topics = ['good morning', 'coffee', 'eye roll','crying','laughing', 'chocolate', 'running',];
 
-$('#addTopic').on('click', function(event){
+// Functions ----------------------------------------------
 
-    event.preventDefault();
-    
-    var newTopic=$('#inlineFormInputName2').val().trim();
-    
-    console.log(newTopic);
-    topics.push(newTopic);
-    displayButtons();
-});
+    function displayButtons() {
+    //add buttons to page dynamically:  loop through array creating a button for each element in array
 
-
-
-
-
-
-// Function:  displayButtons
-//add buttons to page dynamically:  loop through array creating a button for each element in array
-function displayButtons() {
-
-
-    // clear out old buttons and replace with current
-    $('#topic-buttons').empty();
+        // clear out old buttons and replace with current
+        $('#topic-buttons').empty();
 
         for(var i=0; i<topics.length; i++) {
         // * append new <button> to <div> containing buttons
@@ -41,90 +26,99 @@ function displayButtons() {
             B.text(topics[i]);
 
             $('#topic-buttons').append(B);
+         }
     }
-}
-displayButtons();
-//get user input:  what type of gif requested? --- Event listener
-//remove spaces/blanks --- .val().trim()
-$(document).on('click', '.gtopic',function(){
-
-    newGiphy = $(this).attr('data-name');
     
-    getYerImgs();
-})
 
-$(document).on('click', '.giphy', function(){
-
-    var state=$(this).attr("data-state");
-    console.log(state);
-    //change state of image from still to animate
-    if (state === "still"){
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else{
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
-//API Info:
-//  Giphy API key:  AawRiCsMDzpjKTtiI0LQ0pEmQ1bw7kLd
-
-//Example code
-//var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-//xhr.done(function(data) { console.log("success got data", data); });
-
-// ajax query Giphy : send request -- GET 10 images of chosen keyword
-function getYerImgs(){
-    console.log('newGiphy: ' + newGiphy);
-    var apiKey= "AawRiCsMDzpjKTtiI0LQ0pEmQ1bw7kLd";
-    var queryURL= "https://api.giphy.com/v1/gifs/search?q=" + newGiphy + "&rating=g&rating=pg&api_key=" + apiKey + "&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        
-        $('#giphy-display').empty();
-
-        //  Receive/process returned Giphy data
-        var results = response.data; 
+    function getYerImgs(){
+    // API :  ajax query Giphy -- send request/GET 10 images of chosen keyword
     
-        //  Create HTML for each image returned 
-        for (var i=0; i<results.length; i++) {
-            //Make div tag
-            var giphyDiv = 
-            $('<div>').addClass('.card giphy-display');
+        //Giphy key and URL
+        var apiKey= "AawRiCsMDzpjKTtiI0LQ0pEmQ1bw7kLd";
+        var queryURL= "https://api.giphy.com/v1/gifs/search?q=" + newGiphy + "&rating=g&rating=pg&api_key=" + apiKey + "&limit=10";
 
-            //Make image tag for div
-            var giphyImg = $('<img>');
-            // Add attributes for still and animated versions, set state to still
-            giphyImg.attr({
-                src: results[i].images.fixed_height_still.url,
-                'data-state': "still",
-                'data-still': results[i].images.fixed_height_still.url,
-                'data-animate': results[i].images.fixed_height.url,
-                class: "giphy",
-            });
-            // Make paragraph tag for rating 
-            var giphyRating = $('<p>').text('Rated: ' + results[i].rating);
+        // ajax request to Giphy ---
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+    
+            // clear giphy images currently on page
+            $('#giphy-display').empty();
+
+            //  Receive/process returned Giphy data
+            var results = response.data; 
+    
+            //  Create HTML for each image returned 
+            for (var i=0; i<results.length; i++) {
+                //Make div tag
+                var giphyDiv = 
+                $('<div>').addClass('.card giphy-display');
+
+                //Make image tag for div
+                var giphyImg = $('<img>');
+                // Add attributes for still and animated versions in object format, set state to still
+                giphyImg.attr({
+                    src: results[i].images.fixed_height_still.url,
+                    'data-state': "still",
+                    'data-still': results[i].images.fixed_height_still.url,
+                    'data-animate': results[i].images.fixed_height.url,
+                    class: "giphy",
+                });
+                // Make paragraph tag to hold rating 
+                var giphyRating = $('<p>').text('Rated: ' + results[i].rating);
             
-            giphyDiv.append(giphyImg);
-            giphyDiv.append(giphyRating);
+                //Add new tags to new Div tag
+                giphyDiv.append(giphyImg);
+                giphyDiv.append(giphyRating);
 
-            $('#giphy-display').append(giphyDiv);
-        }
+                //Add new HTML for new giphy to existing page
+                $('#giphy-display').append(giphyDiv);
+            }
+        });//---end of ajax response --
+    }
+//  -----------------------------------------------------------
 
+    // Dynamically generate buttons
+    displayButtons();
 
-    });
-}
-// React to user input:  
+    //Listen for user input:
 
-// 1.-- display 10 images along with rating
+        // NEW BUTTON: Add new topics from user input
+        $('#addTopic').on('click', function(event){
 
-//animated gif:  
-    // user click to start animation -- continue as infinite loop
-    // user click to end
+            // Code to accept ENTER button as submit   
+            event.preventDefault();
 
-// 2. Add user keyword to topics array and add to buttons (call displayButtons)
+            // Get user input from form and omit spaces
+            var newTopic=$('#inlineFormInputName2').val().trim();
+    
+            // Add new topic to end of array and call function to display buttons 
+            topics.push(newTopic);
+            displayButtons();
+        });
+
+        // GET GIFs 
+         $(document).on('click', '.gtopic',function(){
+
+            newGiphy = $(this).attr('data-name');
+    
+            getYerImgs();
+        });
+
+        // CHANGE STATE:  still to animate & vv
+        $(document).on('click', '.giphy', function(){
+
+            var state=$(this).attr("data-state");
+            console.log(state);
+            //change state of image from still to animate
+            if (state === "still"){
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else{
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
 
 });
